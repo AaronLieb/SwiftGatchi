a736ff11d4f79bf67dace8eec6096c2adfafaeba
AaronLieb <aarnliebrmn@gmail.com>
2020-10-12 02:33:36 -0700

    animated bars

--- Sources/SwiftGotchi/main.swift
+++ Sources/SwiftGotchi/main.swift
@@ -1,6 +1,8 @@
 import ArgumentParser
 import Foundation
 
+let WINDOW_HEIGHT = 10
+
 struct Gotchi: ParsableCommand {
 	@Option(name: .shortAndLong, help: "The number of minutes between ticks.")
 	var time : Double = 5
@@ -47,24 +49,37 @@ class Pet {
 		display()
 		if (hunger <= 0) { die() }
 	}
-func feed() {
+
+	func feed() {
 		hunger += 0.2
 	}
-func display() {
+
+	func bar(height: Int, value: Int, color: Color) -> [String] {
+		var rows : [String] = [Color.white + "┌─┐"]
+		for i in (0..<height-2).reversed() {
+			rows.append("│" + color + "\(value > i ? "█" : "▒")" + Color.white + "│")
+		}
+		rows.append(Color.white + "└─┘")
+		return rows
+	}
+
+	func display() {
 		let num = Int(hunger/0.1)
-		//let str = String(repeating: "+", count: num)
-		print("\rHunger: \(hunger)              ", terminator:"")
+		let hungerBox = bar(height: WINDOW_HEIGHT, value: num, color: Color.red)
+		let fitnessBox = bar(height: WINDOW_HEIGHT, value: num, color: Color.green)
+		print(bcat([hungerBox, fitnessBox]) ?? "Failed", terminator: "")
+		print("\u{001B}[\(WINDOW_HEIGHT)A", terminator:"")
 		fflush(stdout)
-		print("\u{33}[2K;")
 	}
 
 	func die() {
+		print("\u{001B}[\(WINDOW_HEIGHT)B", terminator:"")
 		DispatchQueue.main.async {self.alive = false}
 		print("RIP \(name)")
 		print("2020 - 2020")
 
 	}
-
 }
+
 
 Gotchi.main()
